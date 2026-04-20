1ab983459c614a93b7c705362b8a7d26557745f5
RevenueOxygen <panlilioa50345@masseyhigh.school.nz>
2026-04-20 12:13:03 +1200

    Statement Generator
    This is a C_01- wait, C_01??? IS THAT A C_01 PERMIT FROM SUPER EARTH REFERENCE??? SUPER EARTH??? IS THAT A HELLDIVERS REFERENCE???? HELLDIVERS?!? IS THAT A HELLDIVERS 2 REFERENCE?????

---
+++ C_01_Statement_Generator.py
@@ -0,0 +1,7 @@
+# functions go here wowie
+
+def statement_generator(statement,decoration):
+    print(f"\n{decoration * 5} {statement} {decoration * 5}")
+
+#Main Routine Here
+statement_generator("Instructions", "-")
